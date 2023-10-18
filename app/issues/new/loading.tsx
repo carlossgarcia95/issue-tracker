9be83ec1e19748d1cfab3be84df129153612ce8f
Carlos Garcia <carlosgarcia3995@gmail.com>
2023-10-18 15:41:36 -0700

    Disable SSR for loading md editor

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,10 +1,8 @@
 import { Box } from "@radix-ui/themes";
 import "react-loading-skeleton/dist/skeleton.css";
 import Skeleton from "react-loading-skeleton";
-import delay from 'delay'
 
 const LoadingNewIssuePage = async () => {
-    await delay(2000)
   return (
     <Box className="max-w-l">
       Loading...
