bc68a7fdd902327d4390b105d4e252b8868590e6
Carlos Garcia <carlosgarcia3995@gmail.com>
2023-10-20 12:56:42 -0700

    Add user profile image and dropdown menu

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -6,7 +6,14 @@ import React from "react";
 import { AiFillBug } from "react-icons/ai";
 import classNames from "classnames";
 import { useSession } from "next-auth/react";
-import { Box, Container, Flex } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 
 const Navbar = () => {
   // Browser API
@@ -48,7 +55,25 @@ const Navbar = () => {
             {" "}
             <Box>
               {status === "authenticated" && (
-                <Link href={"/api/auth/signout"}>Singout</Link>
+                <DropdownMenu.Root>
+                  <DropdownMenu.Trigger>
+                    <Avatar
+                      src={session.user!.image!}
+                      fallback="?"
+                      size={"2"}
+                      radius="full"
+                      className="cursor-pointer"
+                    />
+                  </DropdownMenu.Trigger>
+                  <DropdownMenu.Content>
+                    <DropdownMenu.Label>
+                      <Text size={"2"}>{session.user!.email}</Text>
+                    </DropdownMenu.Label>
+                    <DropdownMenu.Item>
+                      <Link href={"/api/auth/signout"}>Singout</Link>
+                    </DropdownMenu.Item>
+                  </DropdownMenu.Content>
+                </DropdownMenu.Root>
               )}
               {status === "unauthenticated" && (
                 <Link href={"/api/auth/signin"}>Login</Link>
