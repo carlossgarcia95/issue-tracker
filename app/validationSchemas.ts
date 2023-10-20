769129be8c4ef88cdfa6f1b65e7c39463b6795d9
Carlos Garcia <carlosgarcia3995@gmail.com>
2023-10-20 16:16:49 -0700

    Show toast notifications

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,10 +1,12 @@
 import { z } from "zod";
 
+// Schema for creating a new issue
 export const issueSchema = z.object({
   title: z.string().min(1, "Title is required.").max(255),
   description: z.string().min(1, "Description is required.").max(65535),
 });
 
+// Schema for updating an issue or assigning user to issue
 export const patchIssueSchema = z.object({
   title: z.string().min(1, "Title is required.").max(255).optional(),
   description: z
