0c2f79d6580208177912449cfc317df4d318b1b8
Carlos Garcia <carlosgarcia3995@gmail.com>
2023-10-23 10:26:54 -0700

    Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -26,10 +26,17 @@ const IssuesPage = async ({ searchParams }: Props) => {
     ? searchParams.status
     : undefined;
 
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: {
-      status: status,
+      status,
     },
+    orderBy,
   });
 
   return (
@@ -47,7 +54,9 @@ const IssuesPage = async ({ searchParams }: Props) => {
                 >
                   {column.label}
                 </NextLink>
-                {column.value === searchParams.orderBy && <ArrowUpIcon className="inline"/>}
+                {column.value === searchParams.orderBy && (
+                  <ArrowUpIcon className="inline" />
+                )}
               </Table.ColumnHeaderCell>
             ))}
           </Table.Row>
