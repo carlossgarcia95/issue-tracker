bfb9836572c338ad70da0fef2d474763a40ff94e
Carlos Garcia <carlosgarcia3995@gmail.com>
2023-10-24 13:05:16 -0700

    Make IssueStatusBadge selectable

--- app/components/IssueStatusBadge.tsx
+++ app/components/IssueStatusBadge.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import { Status } from "@prisma/client";
-import { Badge } from "@radix-ui/themes";
+import { Select, Badge } from "@radix-ui/themes";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import classNames from "classnames";
 
 const statusMap: Record<
   Status,
@@ -21,7 +26,20 @@ const statusMap: Record<
 
 const IssueStatusBadge = ({ status }: { status: Status }) => {
   return (
-    <Badge color={statusMap[status].color}>{statusMap[status].label}</Badge>
+    <Select.Root defaultValue={status}>
+      <Select.Trigger variant="ghost" />
+      <Select.Content variant="soft">
+        <Select.Group>
+          {Object.keys(statusMap).map((statusKey) => (
+            <Select.Item  key={statusKey} value={statusKey} className="hover:bg-slate-100">
+              <Badge color={statusMap[statusKey as Status].color}>
+                {statusMap[statusKey as Status].label}
+              </Badge>
+            </Select.Item>
+          ))}
+        </Select.Group>
+      </Select.Content>
+    </Select.Root>
   );
 };
 
